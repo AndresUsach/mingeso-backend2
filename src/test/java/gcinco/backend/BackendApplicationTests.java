4f09fccbf3fc82344860639b5cdcff7f9d043c85
Andrés Muñoz <andres.munoz.b@usach.cl>
2018-04-17 17:41:43 -0300

    Fix the code

--- src/test/java/gcinco/backend/BackendApplicationTests.java
+++ src/test/java/gcinco/backend/BackendApplicationTests.java
@@ -1,6 +1,6 @@
 package gcinco.backend;
 
-import org.junit.Test;
+
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
