547cdc544aef0ad81144a5227e3bb5bb44f2c404
Andrés Muñoz <andres.munoz.b@usach.cl>
2018-04-22 21:11:54 -0300

    Test

--- src/test/java/gcinco/backend/controller/ProductControllerTests.java
+++ src/test/java/gcinco/backend/controller/ProductControllerTests.java
@@ -4,12 +4,19 @@ import gcinco.backend.entity.Product;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class ProductControllerTests {
     @Test
-    public void newProduct(){
+    public void newProduct() throws ParseException {
 
         Product product = new Product();
         product.setPrice(100);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = sdf.parse("2021-12-21");
+        product.setExpirationDate(date);
         product.setCategory("Internacional");
         product.setName("Taza");
         product.setId(100);
